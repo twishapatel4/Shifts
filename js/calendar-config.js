62a300ae880b59ecff50449fe19fc47c0899f524
twishapatel4 <twisha@techeniac.com>
2025-11-10 16:08:27 +0530

    fix: Header

--- js/calendar-config.js
+++ js/calendar-config.js
@@ -50,12 +50,31 @@ function initCalendar(resources, events) {
     slotWidth: 220,
     resources,
     events,
-    viewDidMount(info) {
-      addTitleBelowDayHeaders(info);
-    },
+    // viewDidMount(info) {
+    //   addTitleBelowDayHeaders(info);
+    // },
     eventContent: renderEventDetails,
     dayHeaderFormat: CustomHeader,
     resourceLabelContent: renderResources,
+    viewDidMount() {
+      const titleEl = document.querySelector(".ec-sidebar-title");
+      if (titleEl && !titleEl.querySelector(".custom-sidebar-div")) {
+        // Create your custom div
+        const customDiv = document.createElement("div");
+        customDiv.className = "custom-sidebar-div";
+
+        // Add your content inside the div
+        customDiv.innerHTML = ` 
+        <div class="sidebar-title">
+      <div class="sidebar-title-main">November 2025</div>
+      <div class="sidebar-title-sub">Day 0 HRS</div>
+      </div>
+    `;
+
+        // Append the div to the ec-sidebar-title
+        titleEl.appendChild(customDiv);
+      }
+    },
   });
 
   document
@@ -153,10 +172,7 @@ function renderResources(arg) {
           <div class="parent-resource">
           <div class="parent-title-hours">
             <div style="font-size:22px;">${resource.title}</div>
-            <div>${hours} hrs</div>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" height="16" width="16">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z"/>
-            </svg>
+           
           </div>
         </div>`,
     };
@@ -183,16 +199,32 @@ function renderResources(arg) {
 function renderEventDetails(arg) {
   const event = arg.event;
   console.log(event);
-
+  // const resource = resources.find((r) => r.id === event.resourceId);
+  // const hours = resource?.extendedProps?.hours || 0;
+  // console.log(hours);
   if (event.title === "shift-full") {
-    console.log("parent");
     return {
       html: `
-        <div class="parent-event">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6" height="24" width="24">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z" />
-</svg>${"Add People"}
-        </div>
+        <div class="parent-event"><div>hrs
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      height="16"
+      width="16"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z"
+      />
+    </svg></div>
+          <div><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6" height="24" width="24">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z" />
+          </svg>Add People</div>
+          </div>
       `,
     };
   }
