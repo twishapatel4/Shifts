ce0c64ab39d08cd22d4f4f25d449ebdd9785f888
twishapatel4 <twisha@techeniac.com>
2025-12-03 15:30:46 +0530

    feat: Loader

--- js/calendar.js
+++ js/calendar.js
@@ -4,10 +4,7 @@ let calCollapsed = {}; // store collapse state of each calendar section
 const calendarMap = {};
 let activePopupEvent = null;
 
-var X = parent.Xrm;
-
 async function loadShifts() {
-  X.Utility.showProgressIndicator("Loading...");
   const data = {
     resources: [
       {
@@ -265,7 +262,6 @@ async function loadShifts() {
 
   // INITIALIZE CALENDARS DYNAMICALLY
   renderCalendars(groups);
-  X.Utility.closeProgressIndicator();
 }
 
 function renderCalendars(groups) {
